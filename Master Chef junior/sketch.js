6149dd0cb42689049d4c4e206d554b9e044dd155
ThrinayR <62817945+ThrinayR@users.noreply.github.com>
2021-08-28 09:27:40 -0500

    Add files via upload

---
+++ Master Chef junior/sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  
+  
+
+}
+
+function draw() {
+
+
+console.log("to make pancakes, youmix pancake batter from the store with mile and eggs, mix it with a whisk, and put over low heat for about 5 minutes")
+
+
+
+
+
+drawSprites();
+}
